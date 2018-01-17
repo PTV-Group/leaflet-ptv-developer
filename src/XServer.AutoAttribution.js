c2257e528e1597df0782406bdfa3638d929bca2f
oliverheilig <oliver.heilig@yahoo.de>
2018-01-17 15:06:52 +0100

    added support for FeatureLayer attribution

--- src/XServer.AutoAttribution.js
+++ src/XServer.AutoAttribution.js
@@ -9,7 +9,10 @@
 	var proto = L.TileLayer.prototype;
 	var prev = proto.initialize;
 
-	var xmapRegex = new RegExp('(^http[s]:\/\/.*\/)services\/(rest|rs)\/XMap\/');
+	// match fro xMap-2 /rest/ and /rs/ urls
+	var xmapRegex = new RegExp('(^https*:\/\/.*\/)services\/(rest|rs)\/XMap\/');
+
+	// match for xserver-internet token (only new GUID-tokens!)
 	var tokenRegex = new RegExp('[&\?]xtok=(\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12})');
 
 	proto.initialize = function (url, options) {
@@ -23,16 +26,17 @@
 			z: 0
 		});
 
+		// does it match an xMap-2 url?
 		var urlMatch = xmapRegex.exec(resolvedUrl);
-
 		if (!urlMatch || urlMatch.length < 2)
 			return;
+
+		// use the same host for XRuntime
+		var host = urlMatch[1];
 
 		// parse token from resolvedUrl
 		var tokenMatch = tokenRegex.exec(resolvedUrl);
 		var token = (tokenMatch && tokenMatch.length > 1) ? tokenMatch[1] : null;
-
-		var host = urlMatch[1];
 
 		// get copyright from XRuntime
 		var req = superagent.post(host + 'services/rs/XRuntime/experimental/getDataInformation')
@@ -50,23 +54,53 @@
 			req.auth('xtok', token);
 
 		var that = this;
-		this, req.end(function (err, resp) {
-			var copyright = err || !resp || !resp.body || !resp.body.mapDescription ||
-				!resp.body.mapDescription.providerInformation ?
-				'PTV, HERE or maybe TOMTOM, AND' :
-				'PTV, ' + resp.body.mapDescription.providerInformation;
-			
+		req.end(function (err, resp) {
 			var oldCopyright = that.options.attribution;
 
-			that.options.attribution = copyright;
+			var newCopyright = (err || !resp || !resp.body || !resp.body.mapDescription ||
+					!resp.body.mapDescription.copyright) ?
+				'PTV, HERE (or maybe TOMTOM), AND' :
+				matchCopyrights(resolvedUrl, resp.body.mapDescription.copyright);
+
+			that.options.attribution = newCopyright;
 
 			// set or replace copyright in attributionControl
-			if (that._map.attributionControl) {
-				if(oldCopyright) // remove old copyright
+			if (that._map && that._map.attributionControl) {
+				if (oldCopyright) // remove old copyright
 					that._map.attributionControl.removeAttribution(oldCopyright);
 
-				that._map.attributionControl.addAttribution(copyright);
+				that._map.attributionControl.addAttribution(newCopyright);
 			}
 		});
+	}
+
+	function matchCopyrights(url, copyright) {
+		var matchedCopyrights = [];
+
+		// match feature layer copyrights
+		var featurelayerRegex = /(PTV_[A-Za-z]*)/g;
+		if (copyright.featureLayers && copyright.featureLayers > 0) {
+			var featurelayerMatches = url.match(featurelayerRegex);
+			if (featurelayerMatches && featurelayerMatches.length > 0) {
+				copyright.featureLayers.forEach(function (el) {
+					if (featurelayerMatches.indexOf(el.themeId) != -1)
+						matchedCopyrights = matchedCopyrights.concat(el.copyright);
+				});
+			}
+		}
+
+		// match for basemap copyrights
+		var baselayerRegex = new RegExp('(&|\\?)(layers=.*(background|labels|transport)|(?!layers=))');
+		var baselayerMatch = baselayerRegex.exec(url);
+		if (baselayerMatch && baselayerMatch.length > 0 || matchedCopyrights.length == 0) {
+			matchedCopyrights = matchedCopyrights.concat(copyright.basemap? copyright.basemap : copyright);
+		}
+
+		// make mentions unique
+		var result = [];
+		matchedCopyrights.forEach(function (el) {
+			if (result.indexOf(el < 0)) result.push(el);
+		});
+		return result;
 	}
 })();
